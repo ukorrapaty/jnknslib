d22ddcca75717bad03fd1488187eefc7939dec9a
root <root@ip-172-31-48-129.ec2.internal>
2023-01-23 13:16:26 +0000

    test

--- vars/chk.groovy
+++
@@ -1,7 +0,0 @@
-def call (String srcName = 'rebase.py') {
-  final pythonContent = libraryResource("${srcName}")
-  echo "Parameter is : ${srcName}"
-  sh "pwd"
-  echo "${PWD}"
-  sh "${srcName}"
-}
